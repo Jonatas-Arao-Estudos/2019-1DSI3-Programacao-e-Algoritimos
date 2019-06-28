cc874d1713a8034c9a0c1573fb698ca18a3c82fe
Jonatas Arão de Carvalho Neves <jonatas.arao.de.carvalho.neves@hotmail.com>
2019-06-27 21:04:27 -0300

    ProjetoCadastro - Finalizado

--- Aula 23/ProjetoCadastro/ProjetoCadastro/frmPrincipal.Designer.cs
+++ Aula 23/ProjetoCadastro/ProjetoCadastro/frmPrincipal.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.cadastroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.usuarioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -38,6 +39,12 @@
             this.clienteToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.fornecedorToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pdcUsuario = new System.Drawing.Printing.PrintDocument();
+            this.ppdUsuario = new System.Windows.Forms.PrintPreviewDialog();
+            this.pdcCliente = new System.Drawing.Printing.PrintDocument();
+            this.ppdCliente = new System.Windows.Forms.PrintPreviewDialog();
+            this.pdcFornecedor = new System.Drawing.Printing.PrintDocument();
+            this.ppdFornecedor = new System.Windows.Forms.PrintPreviewDialog();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -49,7 +56,7 @@
             this.sairToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(588, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -66,21 +73,21 @@
             // usuarioToolStripMenuItem
             // 
             this.usuarioToolStripMenuItem.Name = "usuarioToolStripMenuItem";
-            this.usuarioToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.usuarioToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
             this.usuarioToolStripMenuItem.Text = "Usuário";
             this.usuarioToolStripMenuItem.Click += new System.EventHandler(this.usuarioToolStripMenuItem_Click);
             // 
             // clienteToolStripMenuItem
             // 
             this.clienteToolStripMenuItem.Name = "clienteToolStripMenuItem";
-            this.clienteToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.clienteToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
             this.clienteToolStripMenuItem.Text = "Cliente";
             this.clienteToolStripMenuItem.Click += new System.EventHandler(this.clienteToolStripMenuItem_Click);
             // 
             // fornecedorToolStripMenuItem
             // 
             this.fornecedorToolStripMenuItem.Name = "fornecedorToolStripMenuItem";
-            this.fornecedorToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.fornecedorToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
             this.fornecedorToolStripMenuItem.Text = "Fornecedor";
             this.fornecedorToolStripMenuItem.Click += new System.EventHandler(this.fornecedorToolStripMenuItem_Click);
             // 
@@ -97,32 +104,82 @@
             // usuarioToolStripMenuItem1
             // 
             this.usuarioToolStripMenuItem1.Name = "usuarioToolStripMenuItem1";
-            this.usuarioToolStripMenuItem1.Size = new System.Drawing.Size(134, 22);
+            this.usuarioToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
             this.usuarioToolStripMenuItem1.Text = "Usuário";
+            this.usuarioToolStripMenuItem1.Click += new System.EventHandler(this.usuarioToolStripMenuItem1_Click);
             // 
             // clienteToolStripMenuItem1
             // 
             this.clienteToolStripMenuItem1.Name = "clienteToolStripMenuItem1";
-            this.clienteToolStripMenuItem1.Size = new System.Drawing.Size(134, 22);
+            this.clienteToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
             this.clienteToolStripMenuItem1.Text = "Cliente";
+            this.clienteToolStripMenuItem1.Click += new System.EventHandler(this.clienteToolStripMenuItem1_Click);
             // 
             // fornecedorToolStripMenuItem1
             // 
             this.fornecedorToolStripMenuItem1.Name = "fornecedorToolStripMenuItem1";
-            this.fornecedorToolStripMenuItem1.Size = new System.Drawing.Size(134, 22);
+            this.fornecedorToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
             this.fornecedorToolStripMenuItem1.Text = "Fornecedor";
+            this.fornecedorToolStripMenuItem1.Click += new System.EventHandler(this.fornecedorToolStripMenuItem1_Click);
             // 
             // sairToolStripMenuItem
             // 
             this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
             this.sairToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
             this.sairToolStripMenuItem.Text = "Sair";
+            this.sairToolStripMenuItem.Click += new System.EventHandler(this.sairToolStripMenuItem_Click);
+            // 
+            // pdcUsuario
+            // 
+            this.pdcUsuario.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.pdcUsuario_PrintPage);
+            // 
+            // ppdUsuario
+            // 
+            this.ppdUsuario.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.ppdUsuario.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.ppdUsuario.ClientSize = new System.Drawing.Size(400, 300);
+            this.ppdUsuario.Document = this.pdcUsuario;
+            this.ppdUsuario.Enabled = true;
+            this.ppdUsuario.Icon = ((System.Drawing.Icon)(resources.GetObject("ppdUsuario.Icon")));
+            this.ppdUsuario.Name = "printPreviewDialog1";
+            this.ppdUsuario.Visible = false;
+            // 
+            // pdcCliente
+            // 
+            this.pdcCliente.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.pdcCliente_PrintPage);
+            // 
+            // ppdCliente
+            // 
+            this.ppdCliente.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.ppdCliente.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.ppdCliente.ClientSize = new System.Drawing.Size(400, 300);
+            this.ppdCliente.Document = this.pdcCliente;
+            this.ppdCliente.Enabled = true;
+            this.ppdCliente.Icon = ((System.Drawing.Icon)(resources.GetObject("ppdCliente.Icon")));
+            this.ppdCliente.Name = "ppdCliente";
+            this.ppdCliente.Visible = false;
+            // 
+            // pdcFornecedor
+            // 
+            this.pdcFornecedor.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.pdcFornecedor_PrintPage);
+            // 
+            // ppdFornecedor
+            // 
+            this.ppdFornecedor.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.ppdFornecedor.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.ppdFornecedor.ClientSize = new System.Drawing.Size(400, 300);
+            this.ppdFornecedor.Document = this.pdcFornecedor;
+            this.ppdFornecedor.Enabled = true;
+            this.ppdFornecedor.Icon = ((System.Drawing.Icon)(resources.GetObject("ppdFornecedor.Icon")));
+            this.ppdFornecedor.Name = "ppdFornecedor";
+            this.ppdFornecedor.Visible = false;
             // 
             // frmPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(588, 307);
+            this.ClientSize = new System.Drawing.Size(284, 133);
+            this.ControlBox = false;
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "frmPrincipal";
@@ -146,6 +203,12 @@
         private System.Windows.Forms.ToolStripMenuItem clienteToolStripMenuItem1;
         private System.Windows.Forms.ToolStripMenuItem fornecedorToolStripMenuItem1;
         private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+        private System.Drawing.Printing.PrintDocument pdcUsuario;
+        private System.Windows.Forms.PrintPreviewDialog ppdUsuario;
+        private System.Drawing.Printing.PrintDocument pdcCliente;
+        private System.Windows.Forms.PrintPreviewDialog ppdCliente;
+        private System.Drawing.Printing.PrintDocument pdcFornecedor;
+        private System.Windows.Forms.PrintPreviewDialog ppdFornecedor;
     }
 }
 
--- Aula 23/ProjetoCadastro/ProjetoCadastro/frmPrincipal.cs
+++ Aula 23/ProjetoCadastro/ProjetoCadastro/frmPrincipal.cs
@@ -12,7 +12,7 @@ namespace ProjetoCadastro
 {
     public partial class frmPrincipal : Form
     {
-        public static int contusu = 0, contcli = 0, contforn = 0, contli = 0, contfor = 0;
+        public static int contusu = 0, contcli = 0, contforn = 0;
         public struct Usuario
         {
             public int cd_usuario;
@@ -33,6 +33,129 @@ namespace ProjetoCadastro
         public static Cliente[] cliente = new Cliente[10];
         public static Fornecedor[] fornecedor = new Fornecedor[10];
 
+        private void pdcUsuario_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            Graphics objImpressao = e.Graphics;
+            string strDados = "";
+            int pag = 0, i = 0, lin = 0;
+            bool cab = true, det = true;
+            while (cab)
+            {
+                pag++;
+                strDados += "Listagem de Usuários\n";
+                strDados += "Página: " + pag.ToString() + "                                                                 ARSOFT\n";
+                strDados += "--------------------------------------------------------------------------------\n";
+                strDados += "Código Nome                                     Nível Login               \n";
+                strDados += "--------------------------------------------------------------------------------\n";
+                lin = 5;
+                det = true;
+                while (det)
+                {
+                    strDados += usuario[i].cd_usuario.ToString("000000") + " " + (usuario[i].nm_usuario + "                                        ").Substring(0, 40) + "   " + usuario[i].sg_nivel + "   " + usuario[i].nm_login + "\n";
+                    i++;
+                    lin++;
+                    if(lin >= 66)
+                    {
+                        det = false;
+                    }
+                    if(i>= contusu)
+                    {
+                        det = false;
+                        cab = false;
+                    }
+                    strDados += (char)12;
+                }
+            }
+            objImpressao.DrawString(strDados, new System.Drawing.Font("Courier New", 10, FontStyle.Bold), Brushes.Black, 50, 50);
+        }
+
+        private void usuarioToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            ppdUsuario.ShowDialog();
+        }
+
+        private void pdcCliente_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            Graphics objImpressao = e.Graphics;
+            string strDados = "";
+            int pag = 0, i = 0, lin = 0;
+            bool cab = true, det = true;
+            while (cab)
+            {
+                pag++;
+                strDados += "Listagem de Clientes\n";
+                strDados += "Página: " + pag.ToString() + "                                                                 ARSOFT\n";
+                strDados += "--------------------------------------------------------------------------------\n";
+                strDados += "Código Nome                                     Nível Login               \n";
+                strDados += "--------------------------------------------------------------------------------\n";
+                lin = 5;
+                det = true;
+                while (det)
+                {
+                    strDados += cliente[i].cd_cliente.ToString("000000") + " " + (cliente[i].nm_cliente + "                                        ").Substring(0, 40) + "   " + cliente[i].sg_nivel + "   " + cliente[i].nm_login + "\n";
+                    i++;
+                    lin++;
+                    if (lin >= 66)
+                    {
+                        det = false;
+                    }
+                    if (i >= contcli)
+                    {
+                        det = false;
+                        cab = false;
+                    }
+                    strDados += (char)12;
+                }
+            }
+            objImpressao.DrawString(strDados, new System.Drawing.Font("Courier New", 10, FontStyle.Bold), Brushes.Black, 50, 50);
+        }
+
+        private void clienteToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            ppdCliente.ShowDialog();
+        }
+
+        private void pdcFornecedor_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            Graphics objImpressao = e.Graphics;
+            string strDados = "";
+            int pag = 0, i = 0, lin = 0;
+            bool cab = true, det = true;
+            while (cab)
+            {
+                pag++;
+                strDados += "Listagem de Fornecedores\n";
+                strDados += "Página: " + pag.ToString() + "                                                                 ARSOFT\n";
+                strDados += "--------------------------------------------------------------------------------\n";
+                strDados += "Código Nome                                     Nível Login               \n";
+                strDados += "--------------------------------------------------------------------------------\n";
+                lin = 5;
+                det = true;
+                while (det)
+                {
+                    strDados += fornecedor[i].cd_fornecedor.ToString("000000") + " " + (fornecedor[i].nm_fornecedor + "                                        ").Substring(0, 40) + "   " + fornecedor[i].sg_nivel + "   " + fornecedor[i].nm_login + "\n";
+                    i++;
+                    lin++;
+                    if (lin >= 66)
+                    {
+                        det = false;
+                    }
+                    if (i >= contforn)
+                    {
+                        det = false;
+                        cab = false;
+                    }
+                    strDados += (char)12;
+                }
+            }
+            objImpressao.DrawString(strDados, new System.Drawing.Font("Courier New", 10, FontStyle.Bold), Brushes.Black, 50, 50);
+        }
+
+        private void fornecedorToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            ppdFornecedor.ShowDialog();
+        }
+
         public frmPrincipal()
         {
             InitializeComponent();
@@ -43,6 +166,12 @@ namespace ProjetoCadastro
             frmUsuario fu = new frmUsuario();
             fu.ShowDialog();
         }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
         private void clienteToolStripMenuItem_Click(object sender, EventArgs e)
         {
             frmCliente fu = new frmCliente();
